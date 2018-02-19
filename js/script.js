2a22649de4b51bf10dd211b62f61ed8555ded4c4
Dhruvv4 <dhruvvaghela17@gmail.com>
2018-02-20 01:38:21 +0530

    css added

--- js/script.js
+++ js/script.js
@@ -21,7 +21,11 @@ $(document).ready(function () {
             var todoText = $(this).val();
             $(this).val("");
             // Create a new li add it to ul
-            $("ul").append("<li><span>X</span> " + todoText +"</li>");
+            $("ul").append('<li><span><i class="fa fa-trash"></i></span>' + todoText +'</li>');
         }
+    });
+
+    $(".fa-plus").click(function () { 
+        $("input[type='text']").fadeToggle();
     });
 });
