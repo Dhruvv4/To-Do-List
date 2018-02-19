d66aebff26b10fac4bc04a32f63c298e23280747
Dhruvv4 <dhruvvaghela17@gmail.com>
2018-02-20 00:22:37 +0530

    Delete functionality added

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
 $(document).ready(function () {
-    
+    $("li").click(function() {
+        $(this).toggleClass("selected");
+    });
+
+    // Click on X to delete todo
+    $("span").click(function(event) {
+        $(this).parent().fadeOut(500, function() {
+            $(this).remove();
+        })
+
+        // This is used to stop the html bubbling.
+        event.stopPropagation();
+    });
 });
